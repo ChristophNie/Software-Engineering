ae1f950c1ef99769b6c186c79277f3920d69c648
Niederer, Christoph <Christoph.Niederer@trumpf.com>
2024-11-27 14:46:47 +0100

    Projektdateien hinzufügen.

---
+++ LiveDemo/MainVM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LiveDemo
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+        private Person person;
+        public Person TargetPerson
+        {
+            get { return person; }
+            set { person = value; OnPropertyChanged(nameof(TargetPerson)); }
+        }
+
+        private ObservableCollection<Person> _persons;
+        public ObservableCollection<Person> Persons
+        {
+            get { return _persons; }
+            set { _persons = value; }
+        }
+
+        public void AddPerson(object person)
+        {
+            Persons.Add(TargetPerson);
+            TargetPerson = new Person();
+        }
+
+        public void RemovePerson(object person)
+        {
+            Persons.Remove((Person)person);
+        }
+
+        public ICommand AddCommand => new RelayCommand(AddPerson, null);
+        public ICommand DeleteCommand => new RelayCommand(RemovePerson, null);
+
+        public MainVM()
+        {
+            Persons = new ObservableCollection<Person>();
+            TargetPerson = new Person();
+        }
+    }
+}
---
+++ LiveDemo/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveDemo
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+        private string _VName;
+        private string _NName;
+        private string _Alter;
+
+        public string VName 
+        { 
+            get { return _VName; }
+            set { _VName = value;  OnPropertyChanged(nameof(VName)); }
+        
+        }
+        public string NName
+        {
+            get { return _NName; }
+            set { _NName = value; OnPropertyChanged(nameof(NName)); }
+
+        }
+        public string Alter
+        {
+            get { return _Alter; }
+            set { _Alter = value; OnPropertyChanged(nameof(Alter)); }
+
+        }
+        public Person()
+        {
+
+        }
+    }
+}
